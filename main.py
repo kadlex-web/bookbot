7dee6f0562025a3bc2ea97fa85cd8ae3a3d11399
kadlex <akulp18@gmail.com>
2024-11-24 12:48:17 -0500

    first attempt at wc function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
-
+        split_file = file_contents.split(" ")
+        print(len(split_file))
 main()
