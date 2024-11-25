fbecb587538b97ddfda0cccfd52282ff7f4718c0
kadlex <akulp18@gmail.com>
2024-11-24 19:52:09 -0500

    refactored main.py, pulled out word count into separate function

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        split_file = file_contents.split()
-        print(len(split_file))
+        num_words = get_word_count(file_contents)
+        print(num_words)
+        
+def get_word_count(file):
+    split_file = file.split()
+    return len(split_file)
+
+
 main()
